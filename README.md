184d6e9a5f4d2ab0c831825c0ebdd93575af1578
John McCarthy <tazerenix@gmail.com>
2024-04-23 17:35:29 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Maths Genealogy API
+
+This is an API which accesses the [Mathematics Genealogy Project](https://genealogy.math.ndsu.nodak.edu/) and returns a directed graph detailing the mathematical genealogy of a given person. 
---
+++ src/main/java/com/example/mathsgenealogyapi/MathsgenealogyapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mathsgenealogyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathsgenealogyapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathsgenealogyapiApplication.class, args);
+	}
+
+}
